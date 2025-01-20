a65be268377996deb41d17baee8c3649a73d11e1
IsaiasTech <isaiasbatistapy@gmail.com>
2025-01-20 12:35:31 -0300

    adicinando listPages

---
+++ lib/pages/list_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class listPage extends StatelessWidget {
+  const listPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(18),
+          child: TextField(
+
+          ),
+
+        ),
+      ) ,
+    );
+  }
+}
