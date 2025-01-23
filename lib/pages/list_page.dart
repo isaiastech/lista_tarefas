317ed511cb5bfc3d6e22009852df91f73c52e7f8
IsaiasTech <isaiasbatistapy@gmail.com>
2025-01-23 11:41:27 -0300

    Criando layout de lista de tarefas no flutter

--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -1,38 +1,87 @@
 import 'package:flutter/material.dart';
 
 class ListPage extends StatelessWidget {
-  ListPage({super.key});
-
-  final TextEditingController emailController = TextEditingController();
+  const ListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(18),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma tarefa',
+                        hintText: 'Ex: Estudar',
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 16,),
+                  ElevatedButton(
+                    onPressed: (){},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff00d7f3),
+                      padding: EdgeInsets.all(14.0),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 35,
+                    ),
+                  ),
+                ],
               ),
-              ElevatedButton(
-                  onPressed: login,
-                  child: Text('Entrar')),
+              SizedBox(height: 18,),
+              // ListView com altura fixa ou Expanded
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  ListTile(
+                    title: Text('Estudar Flutter'),
+                    subtitle: Text('25/03/2025'),
+                    leading: Icon(Icons.save, size: 20,color: Color(0xff1C73ED)),
+                    onTap: (){
+                      print('Você clicou na tarefa');
+                    },
+                  ),
+                  ListTile(
+                    title: Text('Estudar Flutter'),
+                    subtitle: Text('25/03/2025'),
+                    leading: Icon(Icons.personal_injury_rounded, size: 20, color: Color(0xff1C73ED),),
+                    onTap: (){
+                      print('Você clicou na tarefa');
+                    },
+                  ),
+                  // Adicione mais widgets aqui para criar uma lista
+                ],
+              ),
+              SizedBox(height: 18,),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text('Você possui tarefas pendentes'),
+                  ),
+                  SizedBox(width: 8,),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff00d7f3),
+                      padding: EdgeInsets.all(14.0),
+                    ),
+                    child: Text('Limpar tudo'),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
       ),
     );
   }
-
-  void login() {
-    String text = emailController.text;
-    print(text);
-  }
 }
-
